fdd86a69c14b89647e853c93301cdbd4382b4789
lathitien <tienltph19794@fpt.edu.vn>
2023-07-27 23:44:10 +0700

    Missddles

--- src/routers/products.js
+++ src/routers/products.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { addPr, getAllPr, removePr, updatePr } from "../controls/products";
+import { CheckMission } from "../middles/middle";
 
 const router=express.Router();
 
 router.get(`/products`,getAllPr);
-router.post(`/products`,addPr);
-router.put(`/products/:id`,updatePr);
-router.delete(`/products/:id`,removePr);
+router.post(`/products`, CheckMission ,addPr);
+router.put(`/products/:id`, CheckMission,updatePr);
+router.delete(`/products/:id`, CheckMission,removePr);
 
 
 export default router;

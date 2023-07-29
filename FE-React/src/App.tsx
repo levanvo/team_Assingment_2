d2b0a5d9e8ccd7d54b790a61687ec3a9c173254d
Lê Văn Võ <vole543215@gmail.com>
2023-07-29 16:51:59 +0700

    setting router - Home - Management

--- FE-React/src/App.tsx
+++ FE-React/src/App.tsx
@@ -1,16 +1,20 @@
 import { useEffect, useState } from 'react'
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
 import { useAppDispatch, useAppSelector } from "./store/useHookProducts";
 import { addProduct, getProduct, removeProduct, updateProduct } from "./slice/sliceProducts";
 import Home from "./page/Home";
+import Management from './page/Management';
+
+
+
 function App() {
   const dispatch = useAppDispatch();
   const { products, isLoading, error } = useAppSelector((state: any) => state.products);
-  console.log(`App.tsx --> `,products);
-  
+  console.log(`App.tsx --> `, products);
+
   useEffect(() => {
     dispatch(getProduct());
   }, []);
@@ -19,8 +23,8 @@ function App() {
     <div className="">
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home />}></Route>
-
+          <Route path='/' element={<Home />} />
+          <Route path='management' element={<Management />} />
         </Routes>
       </BrowserRouter>
     </div>
